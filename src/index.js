92cbbe3062394b23158a8d56460dcaa0d631a90c
hashmi7917 <hmuqtadeer@gmail.com>
2023-09-23 01:26:04 +0530

    strict-mode

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import React from 'react';
-import { ReactDOM } from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+  return <h1>Hi React App</h1>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
